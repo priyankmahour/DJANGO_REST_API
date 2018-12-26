66cde726e337b658e23827bf8a2c09af11838a20
mahourpriyank <mahourpriyank40@gmail.com>
2018-12-26 12:56:51 +0530

    Partnet Application  Added .. (test.py recieves data from Django APP)

---
+++ withoutRest/test.py
@@ -0,0 +1,8 @@
+import requests
+BASE_URL='http://127.0.0.1:8000/'
+ENDPOINT='jsonapi/'
+resp=requests.get(BASE_URL+ENDPOINT)
+data=resp.json()
+print('#'*50)
+print("DTA FROM DJANGO APP : ")
+print(data)
