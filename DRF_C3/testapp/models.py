d0cf75c1a3e3ef5c94ceda5a17aaea8c628215f8
mahourpriyank <mahourpriyank40@gmail.com>
2019-01-22 00:33:54 +0530

    1.  Models ANd Serializers Created

---
+++ DRF_C3/testapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    eno      =models.IntegerField()
+    ename    =models.CharField(max_length=10)
+    esal     =models.IntegerField()
+    eaddr    =models.CharField(max_length=10)
+
+    
