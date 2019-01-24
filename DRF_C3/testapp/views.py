24acc1af6a43a94bf4e562e5a12ab3be02ccaa40
mahourpriyank <mahourpriyank40@gmail.com>
2019-01-24 22:07:59 +0530

    TWO ENDPOINTS implemented  .... one for ListCreate and other for RetrieveUpdateDestroy

--- DRF_C3/testapp/views.py
+++ DRF_C3/testapp/views.py
@@ -24,38 +24,41 @@ from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,Up
 #     # returning a response is also optional in ListAPIView
 
 
-class EmployeeListAPIView(ListAPIView):
-    #queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    def get_queryset(self):
-        qs=Employee.objects.all()
-        name=self.request.GET.get('ename')
-        if name is not None:
-            qs=qs.filter(ename__icontains=name)
-        return (qs)
-
-class EmployeeCreateAPIView(CreateAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
 
 
-class EmployeeRetrieveAPIView(RetrieveAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-
-
-class EmployeeUpdateAPIView(UpdateAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-
-
-class EmployeeDestroyAPIView(DestroyAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-
+#
+# class EmployeeListAPIView(ListAPIView):
+#     #queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     def get_queryset(self):
+#         qs=Employee.objects.all()
+#         name=self.request.GET.get('ename')
+#         if name is not None:
+#             qs=qs.filter(ename__icontains=name)
+#         return (qs)
+#
+# class EmployeeCreateAPIView(CreateAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#
+#
+# class EmployeeRetrieveAPIView(RetrieveAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+#
+#
+# class EmployeeUpdateAPIView(UpdateAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+#
+#
+# class EmployeeDestroyAPIView(DestroyAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+#
 
 class EmployeeListCreateAPIView(ListCreateAPIView):
     queryset=Employee.objects.all()
@@ -64,20 +67,20 @@ class EmployeeListCreateAPIView(ListCreateAPIView):
     # url pattern does not require id
     # bug fixed
 
-class EmployeeRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-
-
-
-class EmployeeRetrieveDestroyAPIView(RetrieveDestroyAPIView):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
-    lookup_field='id'
-
-
-
+# class EmployeeRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+#
+#
+#
+# class EmployeeRetrieveDestroyAPIView(RetrieveDestroyAPIView):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+#     lookup_field='id'
+#
+#
+#
 
 class EmployeeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset=Employee.objects.all()
