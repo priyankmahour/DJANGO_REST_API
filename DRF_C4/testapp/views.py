3cbdb230d9e32f3871d0d7ee30fa7196062f2168
mahourpriyank <mahourpriyank40@gmail.com>
2019-01-26 20:02:41 +0530

    ReadOnlyModelViewSet  Implemented

--- DRF_C4/testapp/views.py
+++ DRF_C4/testapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
 from .models import Employee
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet,ReadOnlyModelViewSet
 from .serializers import EmployeeSerializer
 
-class EmployeeCRUDCBV(ModelViewSet):
-    # ModelViewSet because it is a viewset on models
+#
+# class EmployeeCRUDCBV(ModelViewSet):
+#     # ModelViewSet because it is a viewset on models
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+
+
+class EmployeeCRUDCBV(ReadOnlyModelViewSet):
+    # ReadOnlyModelViewSet because it provides implementation only for   .list   and    .retrieve
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
