15b63c2a5cc3ce9b2ba1c65034c2489685e16459
mahourpriyank <mahourpriyank40@gmail.com>
2019-02-08 00:03:41 +0530

    LimitOffsetPagination Implemented

--- pagenation/testapp/pagination.py
+++ pagenation/testapp/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
 # Better to create a child class of   PageNumberPagination class with custom our own custom properties for enabling pagenation Locally
 class MyPagination(PageNumberPagination):
     page_size=5
@@ -14,3 +14,15 @@ class MyPagination(PageNumberPagination):
     #  http://127.0.0.1:8000/list-api/?mypage=last will give last page containing 5 records
     last_page_strings=('endpage',)
     #  http://127.0.0.1:8000/list-api/?mypage=endpage will give last page containing 5 records
+
+class MyPagination2(LimitOffsetPagination):
+    #pass # http://127.0.0.1:8000/list-api/    will give 10 records from beginning
+         # default limit is PAGE_SIZE declared globallly in settings .py
+         # defalut offset is the first record available in database
+
+    # http://127.0.0.1:8000/list-api/?limit=13&offset=20
+    default_limit=15   # http://127.0.0.1:8000/list-api/?offset=20     will give 21 to 35
+    limit_query_param='mylimit'
+    offset_query_param='myoffset'
+    max_limit=20
+    # http://127.0.0.1:8000/list-api/?mylimit=25&myoffset=40     will givr 41 to 60
--- pagenation/testapp/views.py
+++ pagenation/testapp/views.py
@@ -6,13 +6,13 @@ from rest_framework.generics import ListAPIView
 from .serializers import EmployeeSerializer
 from .models import Employee
 
-from .pagination import MyPagination   
+from .pagination import MyPagination,MyPagination2
    
 class EmployeeListView(ListAPIView):  # pagination concept is only applicable for LIST Operation because all record will be displayed in this operation only
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
     #pagination_class=PageNumberPagination
-    pagination_class=MyPagination
+    pagination_class=MyPagination2
     
        
 
